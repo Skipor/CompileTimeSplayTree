5dcaf1daa1fd1157a312e375c27690d2e584e38e
Vladimir Skipor <vladimirskipor@gmail.com>
2016-03-11 22:15:49 +0300

    remove junk file

--- SplayTree/NewCppClass.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by  Vladimir Skipor on 10/5/13.
-// Copyright (c) 2013 ___FULLUSERNAME___. All rights reserved.
-//
-// To change the template use AppCode | Preferences | File Templates.
-//
-
-
-#include "NewCppClass.h"
